2b39ae873d94be868218a480d244c100ed4e64c5
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-11-25 01:45:43 +0800

    prettier, miscellaneous fix

--- src/routes/recipe-generator/+page.svelte
+++ src/routes/recipe-generator/+page.svelte
@@ -1,11 +1,11 @@
-<script>
-	import RecipeGenerator from './recipe-generator.svelte';
-	import IngredientSelector from './ingredient-selector.svelte';
+<script lang="ts">
+  import RecipeGenerator from "./recipe-generator.svelte";
+  import IngredientSelector from "./ingredient-selector.svelte";
 </script>
 
 <main class="container mx-auto mt-4">
-	<div class="flex justify-center gap-x-4">
-		<IngredientSelector />
-		<RecipeGenerator />
-	</div>
+  <div class="flex justify-center gap-x-4">
+    <IngredientSelector />
+    <RecipeGenerator />
+  </div>
 </main>
