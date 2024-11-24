d7733c94edb85e17015bcc48655cda0d41984bf5
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-11-25 02:15:14 +0800

    made the types more clear

--- src/lib/data/ingredients.svelte.ts
+++ src/lib/data/ingredients.svelte.ts
@@ -1,10 +1,37 @@
-export const ingredients: { [key: string]: string[] } = {
-  diary: ["milk", "cheese", "yogurt"],
-  fish: ["salmon", "tuna", "cod"],
-  meats: ["chicken", "beef", "pork"],
-  vegetables: ["carrot", "broccoli", "spinach"],
-  fruits: ["apple", "banana", "orange"],
-  grains: ["rice", "pasta", "bread"],
+import { Apple, Fish, Ham, Milk, Salad, Sprout, type Icon as IconType } from "lucide-svelte";
+
+type categoriesAndIngredientsType = {
+  [key: string]: {
+    icon: typeof IconType;
+    ingredients: string[];
+  };
+};
+
+export const categoriesAndIngredients: categoriesAndIngredientsType = {
+  diary: {
+    icon: Milk,
+    ingredients: ["milk", "cheese", "yogurt"],
+  },
+  fish: {
+    icon: Fish,
+    ingredients: ["salmon", "tuna", "cod"],
+  },
+  meats: {
+    icon: Ham,
+    ingredients: ["chicken", "beef", "pork"],
+  },
+  vegetables: {
+    icon: Salad,
+    ingredients: ["carrot", "broccoli", "spinach"],
+  },
+  fruits: {
+    icon: Apple,
+    ingredients: ["apple", "banana", "orange"],
+  },
+  grains: {
+    icon: Sprout,
+    ingredients: ["rice", "pasta", "bread"],
+  },
 };
 
 export const selectedIngredients: { [key: string]: { [key: string]: number } } = $state({});
