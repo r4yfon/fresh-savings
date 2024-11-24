b1a27b961bde2028a5d1d74eecea2cb612a7cc4b
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-11-25 01:46:14 +0800

    show selected ingredients in table form

--- src/routes/recipe-generator/ingredient-selector.svelte
+++ src/routes/recipe-generator/ingredient-selector.svelte
@@ -1,61 +1,75 @@
 <script lang="ts">
-	import * as Accordion from '$lib/components/ui/accordion';
+  import * as Accordion from "$lib/components/ui/accordion";
+  import * as Popover from "$lib/components/ui/popover";
+  import { ingredients, selectedIngredients } from "$lib/data/ingredients.svelte";
 
-	const ingredients: { [key: string]: string[] } = {
-		diary: ['cheese', 'bread', 'butter', 'milk', 'eggs'],
-		fish: ['salmon', 'tuna', 'cod', 'mackerel', 'trout'],
-		meats: ['beef', 'pork', 'chicken', 'lamb', 'turkey'],
-		vegetables: ['carrots', 'potatoes', 'onions', 'tomatoes', 'lettuce'],
-		fruits: ['apples', 'oranges', 'bananas', 'grapes', 'strawberries'],
-		grains: ['rice', 'oats', 'barley', 'wheat', 'corn']
-	};
+  Object.keys(ingredients).forEach((category) => {
+    selectedIngredients[category] = {};
+  });
 
-	let selectedIngredients: { [key: string]: { [key: string]: number } } = $state({
-		diary: {},
-		fish: {},
-		meats: {},
-		vegetables: {},
-		fruits: {},
-		grains: {}
-	});
+  const subtractIngredient = (category: string, ingredient: string) => () => {
+    if (selectedIngredients[category][ingredient] > 0) {
+      selectedIngredients[category][ingredient]--;
+    }
+    if (selectedIngredients[category][ingredient] === 0) {
+      delete selectedIngredients[category][ingredient];
+    }
+  };
+
+  const addIngredient = (category: string, ingredient: string) => () => {
+    if (selectedIngredients[category][ingredient]) {
+      selectedIngredients[category][ingredient]++;
+    } else {
+      selectedIngredients[category][ingredient] = 1;
+    }
+  };
 </script>
 
 <aside class="w-1/4 bg-slate-100">
-	<div class="rounded-md border p-4">
-		<h1 class="mb-2 text-2xl font-bold">ingredient-selecter</h1>
-		<button class="rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700"
-			>use ingredients from inventory</button
-		>
-		<p class="mt-4 w-full border-t-2 pt-2">select ingredients from dropdown</p>
-		<Accordion.Root>
-			{#each Object.keys(ingredients) as category}
-				<Accordion.Item value={category}>
-					<Accordion.Trigger>{category}</Accordion.Trigger>
-					<Accordion.Content>
-						{#each ingredients[category] as ingredient}
-							<button
-								class="rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700"
-								onclick={() => (selectedIngredients[category][ingredient] = 1)}
-							>
-								{ingredient}
-								{#if selectedIngredients[category][ingredient]}
-									<span class="ml-2 rounded-md bg-white px-2 py-0.5 text-emerald-800">
-										{selectedIngredients[category][ingredient]}
-									</span>
-								{/if}
-							</button>
-						{/each}
-					</Accordion.Content>
-				</Accordion.Item>
-			{/each}
-		</Accordion.Root>
-	</div>
+  <div class="rounded-md border p-4">
+    <h1 class="mb-2 text-2xl font-bold">ingredients selecter</h1>
+    <button class="rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700"
+      >use ingredients from inventory</button>
+    <p class="mt-4 w-full border-t-2 pt-2">or select ingredients below</p>
+    <Accordion.Root>
+      {#each Object.keys(ingredients) as category}
+        <Accordion.Item value={category}>
+          <Accordion.Trigger>{category}</Accordion.Trigger>
+          <Accordion.Content class="flex flex-wrap gap-1">
+            {#each ingredients[category] as ingredient}
+              <Popover.Root>
+                <Popover.Trigger>
+                  <button
+                    class="rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700">
+                    {ingredient}
+                    {#if selectedIngredients[category][ingredient]}
+                      <span class="ml-2 rounded-md bg-white px-2 py-0.5 text-emerald-800">
+                        {selectedIngredients[category][ingredient]}
+                      </span>
+                    {/if}
+                  </button>
+                </Popover.Trigger>
+                <Popover.Content class="w-max">
+                  <span>{ingredient}: </span>
+                  <button
+                    class="rounded-md bg-emerald-800 px-2 py-1 text-white hover:bg-emerald-700"
+                    onclick={subtractIngredient(category, ingredient)}>-</button>
+                  <span class="mx-1">
+                    {#if selectedIngredients[category][ingredient]}
+                      {selectedIngredients[category][ingredient]}
+                    {:else}
+                      0
+                    {/if}
+                  </span>
+                  <button
+                    class="rounded-md bg-emerald-800 px-2 py-1 text-white hover:bg-emerald-700"
+                    onclick={addIngredient(category, ingredient)}>+</button>
+                </Popover.Content>
+              </Popover.Root>
+            {/each}
+          </Accordion.Content>
+        </Accordion.Item>
+      {/each}
+    </Accordion.Root>
+  </div>
 </aside>
-
-<style>
-	:global(.recipe-generator-content) {
-		display: flex;
-		flex-wrap: wrap;
-		gap: 0.25rem;
-	}
-</style>
--- src/routes/recipe-generator/recipe-generator.svelte
+++ src/routes/recipe-generator/recipe-generator.svelte
@@ -1,15 +1,46 @@
 <script lang="ts">
-	let selectedIngredients = null;
+  import { selectedIngredients } from "$lib/data/ingredients.svelte";
 </script>
 
 <section class="w-3/4">
-	<div class="w-full">
-		{#if selectedIngredients === null}
-			<h1 class="text-2xl font-bold">Select Ingredients</h1>
-		{:else}
-			efiowffijhello efiowffijhello efiowffijhello efiowffijhello efiowffijhello efiowffijhello
-			efiowffijhello efiowffijhello efiowffijhello efiowffijhello efiowffijhello efiowffijhello
-			efiowffijhello efiowffijhello efiowffijhello
-		{/if}
-	</div>
+  <div class="w-full">
+    {#if Object.values(selectedIngredients).some((category) => Object.keys(category).length > 0)}
+      selected ingredients:
+      <table class="mx-auto mt-2 w-1/2 border text-center">
+        <thead>
+          <tr>
+            <th class="border p-0.5">category</th>
+            <th class="border p-0.5">ingredient</th>
+            <th class="border p-0.5">quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each Object.keys(selectedIngredients) as category}
+            {#each Object.keys(selectedIngredients[category]) as ingredient, index}
+              <tr class="border">
+                {#if index === 0}
+                  <td rowspan={Object.keys(selectedIngredients[category]).length}>{category}</td>
+                {/if}
+                <td class="border">
+                  {ingredient}
+                </td>
+                <td class="border">
+                  {selectedIngredients[category][ingredient]}
+                </td>
+              </tr>
+            {/each}
+          {/each}
+        </tbody>
+      </table>
+
+      <button
+        class="float-right rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700">
+        generate recipes
+      </button>
+    {:else}
+      <div>
+        <p class="text-center">no ingredient selected</p>
+      </div>
+    {/if}
+  </div>
 </section>
