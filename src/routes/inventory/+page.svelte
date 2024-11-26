fb4bb9698b1f1292381f8b90ee117a5a2d7def7f
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-11-27 03:47:05 +0800

    started on inventory page

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -1,3 +1,43 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { inventory } from "$lib/data/inventory.svelte";
+
+  let displayedCategoriesAndIngredients: { [key: string]: { [key: string]: string | number } };
+  displayedCategoriesAndIngredients = inventory;
+</script>
+
 <section class="container mt-4">
   <h1>inventory</h1>
+  {JSON.stringify(inventory)}
+  <table>
+    <thead>
+      <tr>
+        <th class="border">Category</th>
+        <th class="border">Ingredient</th>
+        <th class="border">Quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each Object.keys(displayedCategoriesAndIngredients) as category}
+        {#each Object.keys(displayedCategoriesAndIngredients[category]) as ingredient, index}
+          <tr class="border">
+            {#if index === 0}
+              <td
+                rowspan={Object.keys(displayedCategoriesAndIngredients[category]).length}
+                class="px-1 py-0.5">{category}</td>
+            {/if}
+            <td class="border px-1 py-0.5">
+              {ingredient}
+            </td>
+            <td class="border px-1 py-0.5">
+              {displayedCategoriesAndIngredients[category][ingredient]}
+            </td>
+          </tr>
+        {/each}
+      {/each}
+    </tbody>
+  </table>
+
+  <button class="float-end rounded-md bg-emerald-800 px-4 py-2 text-white hover:bg-emerald-700"
+    >add new ingredient</button>
 </section>
