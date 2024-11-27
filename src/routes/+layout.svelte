a8947181ca66ae031e3d583f4df1784d4e7e6c07
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-11-27 19:16:48 +0800

    dark mode switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import NavBar from '$lib/NavBar.svelte';
-	import '../app.css';
-	let { children } = $props();
+  import NavBar from "$lib/NavBar.svelte";
+  import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
+
+  let { children } = $props();
 </script>
 
+<ModeWatcher />
 <NavBar />
 
 {@render children()}
