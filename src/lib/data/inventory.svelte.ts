e524529eda182a3b2447d7b0b44d1d7ef4346094
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-12-02 22:40:54 +0800

    made inventory object reactive

--- src/lib/data/inventory.svelte.ts
+++ src/lib/data/inventory.svelte.ts
@@ -1,8 +1,8 @@
 import { Apple, Fish, Ham, Milk, Salad, Sprout, type Icon as IconType } from "lucide-svelte";
 
-export const inventory: {
+export let inventory: {
   [key: string]: { [key: string]: { quantity: string | number; expiry: string } };
-} = {
+} = $state({
   diary: {
     milk: { quantity: "2 l", expiry: "2023-12-01" },
     yogurt: { quantity: "50 g", expiry: "2023-11-15" },
@@ -15,7 +15,7 @@ export const inventory: {
     beef: { quantity: "100 g", expiry: "2023-12-05" },
     pork: { quantity: "100 g", expiry: "2023-12-10" },
   },
-};
+});
 
 type categoriesAndIcons = {
   category: string;
